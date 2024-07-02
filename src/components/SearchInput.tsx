cab8dc9f8ec11fd7f5efc962c944ea04a46fd1fd
MiladKhalighi <milad.kh4lighi@gmail.com>
2024-07-02 09:25:19 -0400

    fix serach input navigation

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,21 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { FiSearch } from "react-icons/fi";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (inputRef.current) setSearchText(inputRef.current.value);
+        if (inputRef.current) {
+          setSearchText(inputRef.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup>
